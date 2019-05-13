bb3182fb782d1a81f459ab85fbf6fa5152b5f7d3
MarBry111 <mrmareczek111@gmail.com>
2019-05-13 13:42:42 +0200

    Changed resize to save as text file

--- resize_alphabet.py
+++ resize_alphabet.py
@@ -47,9 +47,10 @@ def reshape_function(im_array=None, filename=None):
     im_strip = im_array[up:down + 1, left:right + 1]
     im = Image.fromarray(im_strip)
 
-    #im.save(filename[:-4] + '.jpeg', "JPEG")
+    # im.save(filename[:-4] + '.jpeg', "JPEG")
 
-    im.resize((8, 8), Image.ANTIALIAS).save(filename[:-4] + '.jpeg', "JPEG")
+    im = im.resize((8, 8), Image.ANTIALIAS)  # .save(filename[:-4] + '.jpeg', "JPEG")
+    return np.asarray(im).reshape(1, 64)
 
 
 folder_name = 'by_class'
@@ -59,11 +60,17 @@ list_folders_1 = os.listdir(folder_name)
 for folder_1 in list_folders_1:
     list_folders_2 = [name for name in os.listdir(folder_name + "/" + folder_1) if os.path.isdir(folder_name + "/" + folder_1 + "/" + name)]
     path_to_save = folder_name + "/" + folder_1 + "/changed"
+    # part to change the center the image at letter and change the sieze to 8x8 pixels
 
     try:
+        # path to save images
         os.mkdir(path_to_save)
+        # path to save text file with values pixels
+        os.mknod(folder_name + "/" + folder_1 + '/' + folder_1 + '.txt')
     except:
         pass
+
+    f_handle = open(folder_name + "/" + folder_1 + '.txt', 'a')
 
     for folder_2 in list_folders_2:
         list_files_3 = [name for name in os.listdir(folder_name + "/" + folder_1 + "/" + folder_2) if os.path.isfile(folder_name + "/" + folder_1 + "/" + folder_2 + "/" + name)]
@@ -72,10 +79,15 @@ for folder_1 in list_folders_1:
             patch_file = folder_name + "/" + folder_1 + "/" + folder_2 + "/" + files_3
             im_array = np.asarray(Image.open(patch_file).convert('L'))
             try:
-                reshape_function(im_array, path_to_save + "/" + files_3)
+                # saves the image file and return the array 1x64 which we save to file
+                save_array = reshape_function(im_array, path_to_save + "/" + files_3)
+                np.savetxt(f_handle, save_array)
             except Exception as ex:
                 print(ex)
+
+    f_handle.close()
+
     # print(patch_file)
-    #plt.imshow(im_array, cmap='gray', vmin=0, vmax=255)
+    # plt.imshow(im_array, cmap='gray', vmin=0, vmax=255)
     # plt.colorbar()
     # plt.show()
